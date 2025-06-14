c490e7d1927a44f941670b8a7efd0052434c7008
Parth Sinha <sinhaparth555@gmail.com>
2025-06-14 09:28:24 +0100

    updated the astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          includePaths: ['./src/styles'],
+          outputStyle: 'compressed',
+        },
+      },
+      minify: true,
+      transform: {
+        minify: true,
+      },
+    },
+    build: {
+      cssMinify: true,
+    }
+  }
+});
